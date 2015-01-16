c1c708f32959152cf5d21747afa0b48c1d11821b
Jorge Casariego <jorgekasa@gmail.com>
2015-01-16 10:44:21 -0300

    First commit

---
+++ app/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('bootstrapApp', ['ui.bootstrap', 'ngRoute']);
+
+//Aqui configuramos las rutas y asociamos cada ruta con una vista y un controlador
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/accordion',{
+			controller: 'AccordionDemoCtrl',
+			templateUrl: '/bootstrap/app/views/accordion.html'
+		})
+		.when('/alert',{
+			controller: 'AlertDemoCtrl',
+			templateUrl: '/bootstrap/app/views/alertDemoCtrl.html'
+		})
+		.otherwise({redirectTo: '/'});
+});
